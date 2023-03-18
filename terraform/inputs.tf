73467b2483c92de72157ac5e85f410f5939e85ec
mpette200 <mpette200@gmail.com>
2023-03-18 21:24:39 +0000

    Create inputs.tf

---
+++ terraform/inputs.tf
@@ -0,0 +1,19 @@
+variable "role_arn" {
+    type = string
+}
+
+variable "session_name" {
+    type = string
+}
+
+variable "web_identity_token_file" {
+    type = string
+}
+
+variable "state_bucket" {
+    type = string
+}
+
+variable "state_key" {
+    type = string
+}
