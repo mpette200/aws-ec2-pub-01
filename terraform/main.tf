f1cb18c641375d070b1e00b5ac150c745d0596b9
mpette200 <mpette200@gmail.com>
2023-03-18 21:24:08 +0000

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "s3" {
+    bucket = var.state_bucket
+    key    = var.state_key
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.56.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  assume_role_with_web_identity {
+    role_arn                = var.role_arn
+    session_name            = var.session_name
+    web_identity_token_file = var.web_identity_token_file
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.19.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+}
